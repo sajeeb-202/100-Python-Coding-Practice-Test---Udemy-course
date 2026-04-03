f29983187de87f497039be9075dd4fba5031436c
sajeeb-202 <sajeebkumar202@gmail.com>
2026-04-03 11:52:26 +0600

    Add Two Numbers

---
+++ .history/Exercise 7_20260403115043.py
@@ -0,0 +1,11 @@
+def add_two_numbers(num1, num2):
+    summ = num1 + num2
+    return summ
+
+#.......Test cases........
+
+Test_values = [(10 , 5), (-4, 9), (0, 6)]
+
+for i in range(len(Test_values)):
+    result = add_two_numbers(*Test_values[i])
+    print(f"{Test_values[i][0]} and {Test_values[i][1]} sum is {Test_values} ")
