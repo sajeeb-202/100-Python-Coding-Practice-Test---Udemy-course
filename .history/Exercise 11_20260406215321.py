e3e9a582b78a33150b7974fc26c9943344e051ef
sajeeb-202 <sajeebkumar202@gmail.com>
2026-04-06 22:12:09 +0600

    Fibonacci Sequence

---
+++ .history/Exercise 11_20260406215321.py
@@ -0,0 +1,23 @@
+def generate_fibonacci(n):
+    Fibonacci_list = []
+    if n<=0:
+        return Fibonacci_list
+        
+    if n==1:
+        return [0]
+        
+    if n==2:
+        return [0,1]
+           
+           
+    if n>2:
+        a = 0
+        b = 1
+        summ= 0
+        Fibonacci_list = [a,b]
+        for I in range(n-2):
+            summ = a+b
+            a, b=b, a+b
+            Fibonacci_list.append(summ)
+            
+        return Fibonacci_list        
---
+++ .history/Exercise 11_20260406215921.py
@@ -0,0 +1,29 @@
+def generate_fibonacci(n):
+    Fibonacci_list = []
+    if n<=0:
+        return Fibonacci_list
+        
+    if n==1:
+        return [0]
+        
+    if n==2:
+        return [0,1]
+           
+           
+    if n>2:
+        a = 0
+        b = 1
+        summ= 0
+        Fibonacci_list = [a,b]
+        for i in range(n-2):
+            summ = a+b
+            a, b=b, a+b
+            Fibonacci_list.append(summ)
+            
+        return Fibonacci_list        
+    
+
+    #.............Test cases.............
+
+    #test_values = int(input())
+    generate_fibonacci(5)
