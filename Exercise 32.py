946aa30f9ea0c45e2f608f7e76a36f31a17e2608
sajeeb-202 <sajeebkumar202@gmail.com>
2026-04-09 16:16:30 +0600

    Generate Fibonacci Sequence Using Recursive Functions

---
+++ Exercise 32.py
@@ -0,0 +1,11 @@
+def fibonacci_recursive(n):
+    if n < 0:
+        return "Invalid input"
+    
+    if n == 0:
+        return 0
+    
+    if n == 1:
+        return 1
+    
+    return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
